c96cc491ac2c3c34862f80ca29af9122e31c6eb0
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-05-29 20:46:55 -0600

    Build is successful -- need to add OAuth2 authentication and authorization system code prior to migration being ran for DB

--- Program.cs
+++ Program.cs
@@ -45,9 +45,7 @@ if (!app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Squashable API V1");
         c.RoutePrefix = "swagger";
     });
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-
-    app.UseHsts();
+  
 }
 
 app.UseHttpsRedirection();
